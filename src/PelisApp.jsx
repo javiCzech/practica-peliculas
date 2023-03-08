4ba9d1e665c41fed0fcde8153fee71290f5374bf
JaviCzech <javier.brizuela237@gmail.com>
2023-03-08 20:34:39 -0300

    Terminada la app de practica

--- src/PelisApp.jsx
+++ src/PelisApp.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { InputCategories } from "./components/InputCategories";
 import { MoviesGrid } from "./components/MoviesGrid";
 
-export const NasaApp = () => {
+export const PelisApp = () => {
     const [categories, setcategories] = useState(['One Piece']);
     const onAddCategory = (newCategory) => {
       if (categories.includes(newCategory)) return
@@ -10,7 +10,7 @@ export const NasaApp = () => {
     }
 
   return <>
-    <h1 className="h1"> NasaApp</h1>
+    <h1 className="h1"> PelisApp</h1>
     <h2 className="h2">Busca tu pelicula aqui</h2>
     <InputCategories onNewCategory={onAddCategory}/>
           
